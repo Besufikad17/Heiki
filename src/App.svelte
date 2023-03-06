7bbf01c67dd3d6017f906ee86435da36f29f0651
besufikad17 <besufikadtialhun1994@gmail.com>
2023-03-06 21:37:20 +0300

    UI updated

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,68 @@
 <script>
   import Header from "./lib/Header.svelte";
   import Footer from "./lib/Footer.svelte";
+  import Card from "./lib/Card.svelte";
+
+  const default_imgs = [
+    "Classic",
+    "Stinger",
+    "Bulldog",
+    "Marshal",
+    "Shorty",
+    "Spectre",
+    "Guardian",
+    "Operator",
+    "Frenzy",
+    "Bucky",
+    "Phantom",
+    "Ares",
+    "Ghost",
+    "Judge",
+    "Vandal",
+    "Odin"
+  ];
 </script>
+<link href='https://fonts.googleapis.com/css?family=Merriweather' rel='stylesheet'>
 
 <main>
-  <Header/>
-  <Footer/>
+  <Header />
+  <div class="container">
+    <div class="row">
+      {#each default_imgs as pic}
+        <Card img="src/assets/{pic}.webp" name={pic} />
+      {/each}
+      <Card img="src/assets/Sheriff.webp" name="Sheriff" />
+      <div />
+      <div />
+      <Card img="src/assets/TacticalKnife.webp" name="Knife" />
+    </div>
+    <br />
+  </div>
+  <Footer />
 </main>
 
 <style>
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 20px;
+    padding: 20px 20px;
+  }
 
+  .row {
+    display: grid;
+    grid-template-columns: repeat(4, 200px);
+    grid-gap: 30px;
+    margin: 10px 10px;
+    padding: 20px 20px;
+    background-color: #141e29;
+  }
+
+  @media (max-width: 800px) {
+    .row {
+      grid-template-columns: repeat(1, 250px);
+    }
+  }
 </style>
---
+++ src/lib/Card.svelte
@@ -0,0 +1,39 @@
+<script>
+    export let img
+    export let name
+</script>
+
+<main>
+  <div class="container">
+    <img src={img} alt="" />
+    <p class="name">{name}</p>
+  </div>
+</main>
+
+<style>
+  .name {
+    display: none;
+    top: 50%;
+    left: 50%;
+    font-family: 'Merriweather';
+    font-size: 11px;
+    color: white;
+  }
+
+  .container:hover {
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    border-style: none;
+    text-align: center;
+    justify-content: center;
+  }
+
+  .container:hover .name {
+    display: block;
+  }
+
+  .container img {
+    width: auto;
+    height: auto;
+    object-fit: cover;
+  }
+</style>
